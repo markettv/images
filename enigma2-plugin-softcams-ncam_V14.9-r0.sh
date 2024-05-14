02e14018bbe87506f744a8b98d063be0623ae4a0
markettv <168855912+markettv@users.noreply.github.com>
2024-05-14 18:01:45 +0100

    Update enigma2-plugin-softcams-ncam_V14.9-r0.sh

--- enigma2-plugin-softcams-ncam_V14.9-r0.sh
+++ enigma2-plugin-softcams-ncam_V14.9-r0.sh
@@ -1,1 +1,19 @@
 
+echo "################ stalker.conf #################"
+echo "############ BY WOLRD OF E2 #################"
+#!/bin/sh
+#
+wget -O /tmp/enigma2-plugin-softcams-ncam_V14.9-r0_all.ipk "https://files.catbox.moe/ep9fes.ipk"
+
+wait
+opkg update && opkg install --force-overwrite /tmp/*.ipk
+
+echo ""
+cd ..
+sync
+echo "############ INSTALLATION COMPLETED ########"
+echo "############ RESTARTING... #################" 
+init 4
+sleep 2
+init 3
+exit 0
